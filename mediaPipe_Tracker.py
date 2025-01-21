28541514626aba9873ad4ab5a711a6e5c10a482e
atifsalam78 <atif.salam@gmail.com>
2025-01-21 23:08:42 +0500

    second commit

--- mediaPipe_Tracker.py
+++ mediaPipe_Tracker.py
@@ -17,7 +17,7 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=False, 
                          max_num_hands=2, 
                          min_detection_confidence=0.5, 
-                         min_tracking_confidence=0.5) # if we go for deault parameters keep it blank
+                         min_tracking_confidence=0.5) # if we go for default parameters keep it blank
 
 mpDraw = mp.solutions.drawing_utils
 
@@ -26,6 +26,7 @@ currentTime = 0
 
 while cap.isOpened():
     success, img = cap.read()
+    img = cv.flip(img, 1)
     ImgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     results = hands.process(ImgRGB)
     print(results.multi_hand_landmarks)
@@ -36,13 +37,16 @@ while cap.isOpened():
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 print(id, cx, cy)
-                if id == 0:
-                    cv.circle(img, (cx, cy), 25, (255, 0, 255), cv.FILLED)
+                if id == 4:
+                    cv.circle(img, (cx, cy), 15, (255, 0, 255), cv.FILLED)
             mpDraw.draw_landmarks(img, handLMS, mpHands.HAND_CONNECTIONS)
     currentTime = time.time()
     fps = 1/(currentTime - startTime)
+    startTime = currentTime
+
+    cv.putText(img, str(int(fps)), (10,70), cv.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
     
 
-    cv.imshow('Image', cv.flip(img, 1))
+    cv.imshow('Image', img)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
