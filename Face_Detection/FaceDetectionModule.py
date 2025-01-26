f14a1fd988ab605f6f0b57ed1a9d471ebaf127b1
atifsalam78 <atif.salam@gmail.com>
2025-01-26 23:09:34 +0500

    add detection scores and values

--- Face_Detection/FaceDetectionModule.py
+++ Face_Detection/FaceDetectionModule.py
@@ -8,7 +8,7 @@ import time
 
 
 class FaceDetector():
-    def __init__(self, model = 0, detectionCon = 0.5):
+    def __init__(self, model = 0, detectionCon = 0.75):
         self.model = model
         self.detectionCon = detectionCon
 
@@ -24,9 +24,9 @@ class FaceDetector():
         self.results = self.face.process(imgRgb)
 
         img.flags.writeable = True
-        if self.results.detections:
-            for detection in self.results.detections:
-                self.mpDraw.draw_detection(img, detection)
+        # if self.results.detections:
+        #     for detection in self.results.detections:
+        #         self.mpDraw.draw_detection(img, detection)
         return img
 
     def findPosition(self, img, draw=True):
@@ -36,7 +36,15 @@ class FaceDetector():
                 # dtList.append([id, detection])
                 # print(id, detection)
                 # print(detection.score)
-                print(detection.location_data.relative_bounding_box)
+                # print(detection.location_data.relative_bounding_box)
+                bboxC = detection.location_data.relative_bounding_box
+                ih, iw, ic = img.shape
+
+                bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                    int(bboxC.width * iw), int(bboxC.height * ih)
+                cv.rectangle(img, bbox, (255, 0, 0), 2)
+                cv.putText(img, f'{int(detection.score[0]*100)}%', (bbox[0], bbox[1]-20),
+                           cv.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (255, 0, 0), 2)
 
 
 
@@ -46,10 +54,22 @@ def main():
     detector = FaceDetector()
 
     cap = cv.VideoCapture(0)
+    # cap = cv.VideoCapture("E:/Codes/videos/11.mp4")
+    # cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)  # Set width to 640 pixels
+    # cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)  # Set height to 480 pixels
+
+    # # Create a named window and resize it
+    # cv.namedWindow("Image", cv.WINDOW_NORMAL)
+    # cv.resizeWindow("Image", 640, 480)
 
     while cap.isOpened():
         sucess, img = cap.read()
+        if not sucess:
+            break
+
         img = cv.flip(img, 1)
+        # img = cv.resize(img, (800,640))
+
         img = detector.findFace(img)
         detector.findPosition(img)
 
@@ -57,7 +77,7 @@ def main():
         fps = 1 / (currentTime - startTime)
         startTime = currentTime
 
-        cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, (0, 255, 0), 3)
+        cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (255, 0, 0), 3)
         
         cv.imshow("Face", img)
 
