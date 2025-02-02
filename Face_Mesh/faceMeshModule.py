d522602b5ab7189dd381faf70b457a41d850d085
atifsalam78 <atif.salam@gmail.com>
2025-02-02 14:25:39 +0500

    update find face mesh method

--- Face_Mesh/faceMeshModule.py
+++ Face_Mesh/faceMeshModule.py
@@ -1,3 +1,7 @@
+import os 
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import tensorflow as tf
+
 import cv2 as cv
 import mediapipe as mp
 import time
@@ -18,21 +22,23 @@ class FaceMeshDetector():
         self.faceMesh = self.mpFaceMesh.FaceMesh(static_image_mode=self.mode, max_num_faces=self.maxFaces,
                                        refine_landmarks=self.refLandmarks, min_detection_confidence=self.detectionCon)
 
-
-    def findFace(self, image, draw=True):
+    def findFaceMesh(self, image, draw=True):
         imgRgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-        results = self.faceMesh.process(imgRgb)
-        if results.multi_face_landmarks:
-            for faceLMS in results.multi_face_landmarks:
+        self.results = self.faceMesh.process(imgRgb)
+        faces = []
+        if self.results.multi_face_landmarks:
+            for faceLMS in self.results.multi_face_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(image, faceLMS, self.mpFaceMesh.FACEMESH_TESSELATION,
+                    self.mpDraw.draw_landmarks(image, faceLMS, self.mpFaceMesh.FACEMESH_CONTOURS,
                                            self.drawSpec, self.drawSpec)
-        return image
-
-    def findPosition(self, image, draw=True):
-        pass
-
-
+                    face = []
+                    for id, lm in enumerate(faceLMS.landmark):
+                        ih, iw, ic = image.shape
+                        x, y, z = int(lm.x*iw), int(lm.y*ih), int(lm.z*ic)
+                        # cv.putText(image, str(id), (x, y), cv.FONT_HERSHEY_PLAIN, 0.50, (0,255,0), 1)
+                        face.append([x, y])
+                    faces.append(face)
+        return image, faces    
 
 def main():
     startTime = 0
@@ -40,22 +46,22 @@ def main():
 
     cap = cv.VideoCapture(0)
 
-    detector = FaceMeshDetector()
-    
-
+    detector = FaceMeshDetector(maxFaces = 2)
 
     while cap.isOpened():
         sucess, image = cap.read()
 
-        image = detector.findFace(image)
+        image, faces = detector.findFaceMesh(image)
+        
         image = cv.flip(image, 1)
+        if len(faces) != 0:
+            print(faces[0])
 
         currentTime = time.time()
         fps = 1 / (currentTime-startTime)
         startTime = currentTime
 
-        cv.putText(image, f'FPS: {int(fps)}', (10,70), cv.FONT_HERSHEY_PLAIN, 3, (0,255,255), 3)
-        
+        cv.putText(image, f'FPS: {int(fps)}', (10,70), cv.FONT_HERSHEY_PLAIN, 3, (0,255,255), 3)  
         
         
         cv.imshow("Face Mesh", image)
